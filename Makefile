7f82abcda8b3cbea97004e064cf97bf935e51139
seojyang <seojyang@student.42seoul.com>
2023-02-18 14:34:29 +0900

    start

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 NAME = minishell
 CC = cc
 CFLAGS = -Wall -Wextra -Werror
-SRCS = mandatory.c
+SRCS = main.c
 
 SRCS_BONUS = bonus.c
 
---
+++ base.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   base.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seojyang <seojyang@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 14:26:25 by seojyang          #+#    #+#             */
+/*   Updated: 2023/02/18 14:34:03 by seojyang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_H
+# define BASE_H
+
+# include <stdio.h> // readline, 
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h> // wait
+# include <readline/history.h>
+# include <readline/readline.h> // readline
+# include <sys/errno.h>
+
+#endif
