3916de319fcb8ac7bddb4c8d6c88be9d7317d5cd
“rolee” <rolee@student.42seoul.kr>
2023-03-25 19:47:05 +0900

    아마도 끝..?

--- srcs/pipe_execute_tree.c
+++ srcs/pipe_execute_tree.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_execute_tree.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seojyang <seojyang@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rolee <rolee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 16:25:31 by rolee             #+#    #+#             */
-/*   Updated: 2023/03/25 17:38:54 by seojyang         ###   ########.fr       */
+/*   Updated: 2023/03/25 19:35:08 by rolee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ static int	execute_brace(t_token *top, t_data *data)
 		exit(exit_status(LOAD));
 	}
 	add_pid(data, pid);
-	if (data->pipe_count == 0 && data->cmd_count == 0)
+	if (data->pipe_count == 0)
 	{
 		if (data->prev_fd != STDIN_FILENO)
 			close(data->prev_fd);
