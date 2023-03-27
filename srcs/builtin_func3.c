0288fc2dde918fbf9fa5bba652fc86238cb26d98
yseojun <ddt1016@naver.com>
2023-03-27 14:37:28 +0900

    cd 수정

--- srcs/builtin_func3.c
+++ srcs/builtin_func3.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_func3.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rolee <rolee@student.42.fr>                +#+  +:+       +#+        */
+/*   By: seojun <seojun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 17:51:38 by rolee             #+#    #+#             */
-/*   Updated: 2023/03/27 12:07:20 by rolee            ###   ########.fr       */
+/*   Updated: 2023/03/27 14:36:49 by seojun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,10 @@ int	builtin_cd(t_data *data, char *dir)
 		if (!dir)
 		{
 			ft_putendl_fd("minishell: cd: HOME not set", STDERR_FILENO);
-			free(dir);
 			return (EXIT_FAILURE);
 		}
+		chdir(dir);
+		free(dir);
 	}
 	else
 	{
