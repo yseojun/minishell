371c3e4019430dd64991434f120dc5d1b6a3f4cd
“rolee” <rolee@student.42seoul.kr>
2023-03-28 11:25:04 +0900

    히어독 기호일 때 ambiguous redirect 처리 안 함

--- srcs/pipe_set.c
+++ srcs/pipe_set.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_set.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seojyang <seojyang@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rolee <rolee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 20:38:32 by seojyang          #+#    #+#             */
-/*   Updated: 2023/03/26 21:30:00 by seojyang         ###   ########.fr       */
+/*   Updated: 2023/03/28 11:20:05 by rolee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,7 +93,8 @@ static int	set_in_fd(t_token *unit, t_data *data)
 	if (!ft_strncmp(unit->token, "<<", 3)
 		|| !ft_strncmp(unit->token, "<", 2))
 	{
-		if (unit->right && (unit->right->right || unit->right->type == ERROR))
+		if (!ft_strncmp(unit->token, "<", 2) && unit->right
+			&& (unit->right->right || unit->right->type == ERROR))
 		{
 			ft_putstr_fd("minishell: ", STDERR_FILENO);
 			ft_putendl_fd("ambiguous redirect", STDERR_FILENO);
